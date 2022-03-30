cd82d288f518e5def228f0d86fe7ad5620db1d25
AbduK03 <abdurahmankhan2003@icloud.com>
2022-03-30 14:40:56 +0000

    exersices and tests passing

--- workspace/tango_with_django_project/rango/urls.py
+++ workspace/tango_with_django_project/rango/urls.py
@@ -4,5 +4,7 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # about page url mapping
+    path('about/', views.about, name='about')
 ]
